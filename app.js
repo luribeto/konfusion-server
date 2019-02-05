fbd1df2ac3ee0a8148535f1b14bc0f2b18007fff
Leonardo Uribe <luribeto@gmail.com>
2019-02-05 15:27:33 -0500

    Express Generator REST API

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishesRouter = require('./routes/dishes');
+var leadersRouter = require('./routes/leaders');
+var promotionsRouter = require('./routes/promotions');
 
 var app = express();
 
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishesRouter);
+app.use('/leaders', leadersRouter);
+app.use('/promotions', promotionsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
