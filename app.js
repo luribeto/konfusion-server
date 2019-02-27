3d873300f1aafa322296852059f515ec5d435991
Leonardo Uribe <luribeto@gmail.com>
2019-02-27 10:47:08 -0500

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ var dishesRouter = require('./routes/dishes');
 var leadersRouter = require('./routes/leaders');
 var promotionsRouter = require('./routes/promotions');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/konfusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server db");
+}, (err) => { console.log(err); });
+
 var app = express();
 
 // view engine setup
