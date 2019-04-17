17ac72124656f43e77d18169fdec6faab5262481
Leonardo Uribe <luribeto@gmail.com>
2019-04-17 16:45:56 -0500

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,7 +22,7 @@ const url = 'mongodb://localhost:27017/konfusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-    console.log("Connected correctly to server db");
+  console.log("Connected correctly to server db");
 }, (err) => { console.log(err); });
 
 var app = express();
@@ -36,36 +38,55 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
 app.use(session({
-    name: 'session-id',
-    secret: '12345-67890-09876-54321',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// V:3
 function auth(req, res, next) {
-    console.log(req.session);
+  console.log(req.user);
 
-    if (!req.session.user) {
-        var err = new Error('You are not authenticated!');
-        err.status = 403;
-        return next(err);
-    }
-    else {
-        if (req.session.user === 'authenticated') {
-            next();
-        }
-        else {
-            var err = new Error('You are not authenticated!');
-            err.status = 403;
-            return next(err);
-        }
-    }
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
+  }
+  else {
+    next();
+  }
 }
 
+// V: 2
+// function auth(req, res, next) {
+//     console.log(req.session);
+
+//     if (!req.session.user) {
+//         var err = new Error('You are not authenticated!');
+//         err.status = 403;
+//         return next(err);
+//     }
+//     else {
+//         if (req.session.user === 'authenticated') {
+//             next();
+//         }
+//         else {
+//             var err = new Error('You are not authenticated!');
+//             err.status = 403;
+//             return next(err);
+//         }
+//     }
+// }
+
+// V: 1
 // function auth (req, res, next) {
 //     console.log(req.session);
 
@@ -117,18 +138,18 @@ app.use('/promotions', promotionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
