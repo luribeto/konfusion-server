4d65f0f0a3d22da82312c575a35c65135cea9768
Leonardo Uribe <luribeto@gmail.com>
2019-04-29 07:36:16 -0500

    Mongoose Population

--- routes/dishes.js
+++ routes/dishes.js
@@ -13,6 +13,7 @@ dishRouter.use(bodyParser.json());
 dishRouter.route('/')
   .get((req, res, next) => {
     Dishes.find({})
+      .populate('comments.author')
       .then((dishes) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -50,6 +51,7 @@ dishRouter.route('/')
 dishRouter.route('/:dishId')
   .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
+      .populate('comments.author')
       .then((dish) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -87,6 +89,7 @@ dishRouter.route('/:dishId')
 dishRouter.route('/:dishId/comments')
   .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
+      .populate('comments.author')
       .then((dish) => {
         if (dish != null) {
           res.statusCode = 200;
@@ -105,12 +108,17 @@ dishRouter.route('/:dishId/comments')
     Dishes.findById(req.params.dishId)
       .then((dish) => {
         if (dish != null) {
+          req.body.author = req.user._id;
           dish.comments.push(req.body);
           dish.save()
             .then((dish) => {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(dish);
+              Dishes.findById(dish._id)
+                .populate('comments.author')
+                .then((dish) => {
+                  res.statusCode = 200;
+                  res.setHeader('Content-Type', 'application/json');
+                  res.json(dish);
+                })
             }, (err) => next(err));
         }
         else {
@@ -162,6 +170,7 @@ dishRouter.route('/:dishId/comments')
 dishRouter.route('/:dishId/comments/:commentId')
   .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
+      .populate('comments.author')
       .then((dish) => {
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
           res.statusCode = 200;
@@ -198,9 +207,13 @@ dishRouter.route('/:dishId/comments/:commentId')
           }
           dish.save()
             .then((dish) => {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(dish);
+              Dishes.findById(dish._id)
+                .populate('comments.author')
+                .then((dish) => {
+                  res.statusCode = 200;
+                  res.setHeader('Content-Type', 'application/json');
+                  res.json(dish);
+                })
             }, (err) => next(err));
         }
         else if (dish == null) {
@@ -217,15 +230,21 @@ dishRouter.route('/:dishId/comments/:commentId')
       .catch((err) => next(err));
   })
   .delete(authenticate.verifyUser, (req, res, next) => {
+    // .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
       .then((dish) => {
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
+
           dish.comments.id(req.params.commentId).remove();
           dish.save()
             .then((dish) => {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(dish);
+              Dishes.findById(dish._id)
+                .populate('comments.author')
+                .then((dish) => {
+                  res.statusCode = 200;
+                  res.setHeader('Content-Type', 'application/json');
+                  res.json(dish);
+                })
             }, (err) => next(err));
         }
         else if (dish == null) {
@@ -241,6 +260,65 @@ dishRouter.route('/:dishId/comments/:commentId')
       }, (err) => next(err))
       .catch((err) => next(err));
   });
+
+
+
+// V before population
+// .put(authenticate.verifyUser, (req, res, next) => {
+//   Dishes.findById(req.params.dishId)
+//     .then((dish) => {
+//       if (dish != null && dish.comments.id(req.params.commentId) != null) {
+//         if (req.body.rating) {
+//           dish.comments.id(req.params.commentId).rating = req.body.rating;
+//         }
+//         if (req.body.comment) {
+//           dish.comments.id(req.params.commentId).comment = req.body.comment;
+//         }
+//         dish.save()
+//           .then((dish) => {
+//             res.statusCode = 200;
+//             res.setHeader('Content-Type', 'application/json');
+//             res.json(dish);
+//           }, (err) => next(err));
+//       }
+//       else if (dish == null) {
+//         err = new Error('Dish ' + req.params.dishId + ' not found');
+//         err.status = 404;
+//         return next(err);
+//       }
+//       else {
+//         err = new Error('Comment ' + req.params.commentId + ' not found');
+//         err.status = 404;
+//         return next(err);
+//       }
+//     }, (err) => next(err))
+//     .catch((err) => next(err));
+// })
+// .delete(authenticate.verifyUser, (req, res, next) => {
+//   Dishes.findById(req.params.dishId)
+//     .then((dish) => {
+//       if (dish != null && dish.comments.id(req.params.commentId) != null) {
+//         dish.comments.id(req.params.commentId).remove();
+//         dish.save()
+//           .then((dish) => {
+//             res.statusCode = 200;
+//             res.setHeader('Content-Type', 'application/json');
+//             res.json(dish);
+//           }, (err) => next(err));
+//       }
+//       else if (dish == null) {
+//         err = new Error('Dish ' + req.params.dishId + ' not found');
+//         err.status = 404;
+//         return next(err);
+//       }
+//       else {
+//         err = new Error('Comment ' + req.params.commentId + ' not found');
+//         err.status = 404;
+//         return next(err);
+//       }
+//     }, (err) => next(err))
+//     .catch((err) => next(err));
+// });
 
 module.exports = dishRouter;
 
