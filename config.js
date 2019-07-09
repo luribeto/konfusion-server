754dd45e2cef88dd01e5405629e144ca5a61d07f
Leonardo Uribe <luribeto@gmail.com>
2019-07-09 10:34:13 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/konfusion'
+  'mongoUrl': 'mongodb://localhost:27017/konfusion',
+  'facebook': {
+    clientId: '654610835054220',
+    clientSecret: 'f8c72687495b0ec287fef0267ba4999b'
+  }
 }
--- routes/users.js
+++ routes/users.js
@@ -167,4 +167,13 @@ router.get('/logout', cors.corsWithOptions, (req, res) => {
   }
 });
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in with facebook!'});
+  }
+});
+
 module.exports = router;
