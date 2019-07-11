5c78142e275ccd91b15f3b92cabdbd1ce97295f4
Leonardo Uribe <luribeto@gmail.com>
2019-07-11 15:46:20 -0500

    herokuy client whitelist 2

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3001', 'https://localhost:3443', 'http://luribe-ltm:3001', 'https://konfusion-server.herokuapp.com/', 'https://konfusion-client.herokuapp.com/', 'https://konfusion-server.herokuapp.com:43058'];
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://localhost:3443', 'http://luribe-ltm:3001', 'https://konfusion-server.herokuapp.com', 'https://konfusion-client.herokuapp.com', 'https://konfusion-server.herokuapp.com:43058'];
 const getCorsOptions = (req) => {
   if (whitelist.indexOf(req.header('Origin')) !== -1) return { origin: true };
   return { origin: false };
@@ -11,6 +11,8 @@ const getCorsOptions = (req) => {
 const corsOptionsDelegate = (req, callback) => {
   console.log(req.header('Origin'));
   const corsOptions = getCorsOptions(req);
+
+  console.log('CORS whitelist>>>>>', whitelist);
 
   // var corsOptions;
   // console.log(req.header('Origin'));
